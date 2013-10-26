e53e2317b93243957a0ecf6543c8f36200ca4683
Vinícius Sales <contato@vinicius-sales.com.br>
2013-10-26 02:10:12 -0200

    Highlight matched and mismatched parenthesis.

--- config/ui.el
+++ config/ui.el
@@ -17,6 +17,10 @@
 ;; Show line numbers
 (global-linum-mode 1)
 
+;; Highlight matched and mismatched parenthesis.
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+
 ;; Load Zenburn color theme.
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'zenburn t)
