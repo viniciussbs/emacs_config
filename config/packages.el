939c6cc2a11a1b6839e3dd864414b83d345d900a
Vinícius Sales <contato@vinicius-sales.com.br>
2014-04-22 00:27:42 -0300

    Adds iy-go-to-char.

--- config/packages.el
+++ config/packages.el
@@ -11,6 +11,13 @@
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (define-key emmet-mode-keymap (kbd "C-j") nil) ;; C-RET is enough
 
+;; iy-go-to-char
+(require 'iy-go-to-char)
+(global-set-key (kbd "C-c s") 'iy-go-to-char)
+(global-set-key (kbd "C-c r") 'iy-go-to-char-backward)
+(global-set-key (kbd "C-c C-s") 'iy-go-to-or-up-to-continue)
+(global-set-key (kbd "C-c C-r") 'iy-go-to-or-up-to-continue-backward)
+
 ;; SCSS mode
 (require 'scss-mode)
 (setq scss-compile-at-save nil)
