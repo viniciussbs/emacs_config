e03fe9d32da54f5806e2f7bab29fcb19dd9688d2
Vinícius Sales <contato@vinicius-sales.com.br>
2014-05-09 00:21:18 -0300

    DONE: multiple-cursors

--- init.el
+++ init.el
@@ -5,6 +5,6 @@
 (load-file "~/.emacs.d/config/ui.el")
 (load-file "~/.emacs.d/config/editor.el")
 (load-file (concat "~/.emacs.d/config/os/" (cond ((eq system-type 'darwin) "osx.el")
-																								 ((eq system-type 'gnu/linux) "linux/el"))))
+																								 ((eq system-type 'gnu/linux) "linux.el"))))
 
 (message "All configurations successfully loaded.")
