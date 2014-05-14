d49f4a7d6a6434bcbf6d842187a5c6002e9352c1
Vinícius Sales <contato@vinicius-sales.com.br>
2014-05-14 15:21:05 -0300

    `s-g` to execute grep in project.

--- config/packages.el
+++ config/packages.el
@@ -62,6 +62,7 @@
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
 (define-key projectile-mode-map [?\s-p] 'projectile-switch-project)
 (define-key projectile-mode-map [?\s-f] 'projectile-find-file)
+(define-key projectile-mode-map [?\s-g] 'projectile-grep)
 
 ;; Lorem Ipsum mode
 (require 'lorem-ipsum)
