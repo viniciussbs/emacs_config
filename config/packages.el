e6f3b31c362a3f3c0bf8df40bdb06e04ba453ef6
Vinícius Sales <contato@vinicius-sales.com.br>
2014-05-14 14:10:41 -0300

    Enable emmet-mode on web-mode.

--- config/packages.el
+++ config/packages.el
@@ -14,6 +14,7 @@
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook  'emmet-mode)
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (define-key emmet-mode-keymap (kbd "C-j") nil) ;; C-RET is enough
 
