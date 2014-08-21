da9bb2b86af46cc9bb741bf453c478448762c378
Vinícius Sales <contato@vinicius-sales.com.br>
2014-08-21 11:56:49 -0300

    Add Lua mode

--- config/packages.el
+++ config/packages.el
@@ -101,3 +101,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;; Lua mode
+(unless (package-installed-p 'lua-mode)
+  (package-install 'lua-mode))
