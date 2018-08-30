84701abe776ba7a874781e5dfd010cd09d87b914
Vinícius <vinicius.sbs@gmail.com>
2018-08-30 10:17:05 -0300

    Define key to toggle block hiding

--- config/editor.el
+++ config/editor.el
@@ -37,3 +37,4 @@
 (global-set-key (kbd "C-|") 'align-regexp)
 (global-set-key "\C-h" 'backward-delete-char-untabify)
 (define-key isearch-mode-map "\C-h" 'isearch-delete-char)
+;; (define-key hs-minor-mode-map (kbd "<backtab>") 'hs-toggle-hiding)
