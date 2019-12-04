9b4082af24235bc5d1abb32151b31bda2f3be840
Vinícius Sales <vinicius.sbs@gmail.com>
2019-12-04 09:56:14 -0300

    things

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("ad79d90546e34078634add073fdc68948f09ae0dfccffffb5294f87a8b862e72" "0496ba3ff126340c6e8c41ca6c8d0136575f51daf127184d8db5cfd929930691" "3a69621a68c2d3550a4c777ffc000e1ea66f5bc2f61112814c591e1bda3f5704" "013e87003e1e965d8ad78ee5b8927e743f940c7679959149bbee9a15bd286689" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "b880872e60d1c7090fcd3d89f287d2a5681e57be9ac90d4682ea442149f5135f" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" default)))
+    ("e9910e2b1fd13eaf308a67062596584d552f9e4940bad73b0e55f5b8ba56a365" "ad79d90546e34078634add073fdc68948f09ae0dfccffffb5294f87a8b862e72" "0496ba3ff126340c6e8c41ca6c8d0136575f51daf127184d8db5cfd929930691" "3a69621a68c2d3550a4c777ffc000e1ea66f5bc2f61112814c591e1bda3f5704" "013e87003e1e965d8ad78ee5b8927e743f940c7679959149bbee9a15bd286689" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "b880872e60d1c7090fcd3d89f287d2a5681e57be9ac90d4682ea442149f5135f" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" default)))
  '(package-selected-packages
    (quote
     (ag diminish eink-theme flycheck-color-mode-line company-graphql company-mode add-node-modules-path exec-path-from-shell graphql-mode monotropic-theme writeroom-mode nord-theme flycheck-credo smartparens avy feature-mode evil yaml-mode web-mode slim-mode scss-mode restclient projectile powerline markdown-mode magit lua-mode lorem-ipsum js2-refactor inf-ruby flycheck flx-ido expand-region emmet-mode))))
