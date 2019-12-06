b495b8e9227e5d760184f4cad1964ed857f6eef3
Vinícius Sales <vinicius.sbs@gmail.com>
2019-12-06 03:32:05 -0300

    More packages

--- init.el
+++ init.el
@@ -19,7 +19,9 @@
 ;; TODO: Use `use-package` or group all variables in a single `setq` call
 ;; https://emacs.stackexchange.com/questions/16477/how-to-use-use-package-with-built-in-packages
 (setq backup-inhibited t)
+(setq auto-save-default nil)
 (setq js-indent-level 2)
+(setq create-lockfiles nil)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
@@ -31,6 +33,18 @@
   :config
   (set-frame-font "Source Code Pro-18")
   (load-theme 'nord t))
+
+(use-package dashboard
+  :ensure t
+  :init
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-items '((recents  . 5)
+			  (projects . 5)
+                          (bookmarks . 5)
+                          (agenda . 5)
+                          (registers . 5)))
+  :config
+  (dashboard-setup-startup-hook))
 
 (use-package flx-ido
   :ensure t
@@ -66,10 +80,25 @@
   :config
   (projectile-mode +1))
 
+(use-package web-mode
+  :ensure t
+  :init
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  (setq web-mode-enable-auto-indentation nil)
+  :mode ("\\.erb\\'" "\\.eex\\'" "\\.hbs\\'" "\\.html?\\'"))
+
 (use-package which-key
   :ensure t
   :config
   (which-key-mode))
+
+(use-package zone-rainbow
+  :ensure t
+  :config
+  (zone-when-idle 120))
 
 
 (custom-set-variables
@@ -77,7 +106,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (js2-mode which-key use-package))))
+ '(package-selected-packages
+   (quote
+    (dashboard fireplace zone-nyan zone-rainbow misc web-mode js2-mode which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
